89138cc0df585c1078c463bd2ca8998ad459b305
Alexandre Ghelfi <alexandreghelfi@gmail.com>
2022-05-01 16:55:21 +0200

    Update file formatting

--- tests/test_annot_checking.py
+++ tests/test_annot_checking.py
@@ -13,6 +13,7 @@ def test_fetch_all_python_files_from_dir():
     assert len(current_dir_py_files) == 1
     assert len(current_dir_py_files_with_init) == 0
 
+
 def test_fetch_all_python_files_from_dir_with_incorrect_input():
     with pytest.raises(TypeError):
         _ = annot_checking.fetch_all_python_files_from_dir(2)
